24b3f3d8f3e04b900d1ee93aede3d65b35be4fa2
ArcidesFerrao <cidesferrao@gmail.com>
2025-08-18 16:12:31 +0200

    mantain border

--- src/components/Languages.tsx
+++ src/components/Languages.tsx
@@ -69,7 +69,7 @@ export const Languages = ({ repos }: LanguagesProps) => {
   }, [repos]);
 
   return (
-    <aside className="languages-aside hide-mobile w-3xs p-4 border rounded-sm border-gray-700">
+    <aside className="languages-aside hide-mobile w-3xs p-4 border rounded-sm border-gray-600">
       <h2 className="text-center">Top 10 Languages</h2>
       {loading ? (
         <SkeletonLanguages />
